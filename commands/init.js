6b96639eca5557d0e49489e3e605f14ca8089b85
Crazy秃头鱼 <65235068+k1148097010@users.noreply.github.com>
2021-12-23 18:43:11 +0800

    feat:创建逻辑为代码

--- commands/init.js
+++ commands/init.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const inquirer = require("inquirer");
+const ncp = require("ncp").ncp;
+const log = require("npmlog");
+const fs = require("fs-extra");
 const Command = require("./command");
 const Package = require("../lib/Package");
 const checkTargetPathExist = require("../lib/checkTargetPathExist");
+const install = require("../lib/utils/install");
 const { getRepoList } = require("../lib/utils/getRepoInfo");
 const { withSpinner } = require("../lib/utils/spinner");
+const constent = require("../lib/constent");
 
 /**
  * @description initCommand创建模板的基本流程
@@ -14,20 +19,34 @@ const { withSpinner } = require("../lib/utils/spinner");
  * ----> 结束
  */
 class InitCommand extends Command {
-  constructor(projectName) {
+  constructor(projectName, prompts) {
     super();
     this.projectName = projectName;
     this.targetPath = this.resolveTargetDir();
-    this.projectInfo = {};
+    this.prompts = prompts || [];
   }
 
   async init(cliOptions) {
-    // 检查目标目录是否存在
-    await checkTargetPathExist(this.targetPath, cliOptions);
-    // 获取远程仓库模板列表
-    // const templateInfo = await this.getTemplateInfo();
-    // 执行创建逻辑
-    await this.create();
+    try {
+      // 检查目标目录是否存在
+      await checkTargetPathExist(this.targetPath, cliOptions);
+      // 获取远程项目模板列表
+      const templateInfo = await this.getTemplateInfo();
+
+      if (templateInfo && templateInfo.length) {
+        // 执行创建
+        await this.create();
+      } else {
+        log.error("暂无可用的模板，请上传后重试~");
+        process.exit(0);
+      }
+      // 安装依赖
+      await install();
+      // 启动项目
+      await startup();
+    } catch (err) {
+      log.error(err);
+    }
   }
 
   resolveTargetDir() {
@@ -35,47 +54,51 @@ class InitCommand extends Command {
     return path.resolve(cwd, this.projectName);
   }
 
-  async getProjectInfo() {
-    const projectInfo = await inquirer.prompt([
-      {
-        name: "description",
-        type: "input",
-        message: "请输入项目描述",
-      },
-      {
-        name: "varsion",
-        type: "input",
-        message: "请输入项目版本",
-      },
-    ]);
-    this.projectInfo = projectInfo;
-  }
-
-  resolveFinalPrompt() {
-    return {
-      ...this.projectInfo,
-    };
-  }
-
   async getTemplateInfo() {
     const repoList = await withSpinner(getRepoList, {
       text: "正在获取远程模板信息，请稍等...",
     })();
-    console.log(repoList);
-
     return repoList;
+  }
+
+  async ask(question) {
+    const answers = await inquirer.prompt(question);
+    return answers;
   }
 
   async create() {
     const _package = new Package({
       name: this.projectName,
     });
+
     if (_package.exist()) {
       await _package.update();
     } else {
       await _package.install();
     }
+    const packageRootPath = _package.getRootPath();
+
+    if (!packageRootPath) {
+      return;
+    }
+
+    if (
+      fs.existsSync(`${packageRootPath}/${constent.CLI_COINFIG_FILE_NAME}.js`)
+    ) {
+      // 如果有自定义配置文件
+      const answers = await this.ask([]);
+      // 拿到结果，通过ejs动态渲染
+    } else {
+      // 直接拷贝到目标目录
+      // ncp(source, destination, function (err) {
+      //   if (err) {
+      //     return console.error(err);
+      //   }
+      //   console.log("done!");
+      // });
+    }
   }
+  async startup() {}
 }
 
 function factory(projectName) {
